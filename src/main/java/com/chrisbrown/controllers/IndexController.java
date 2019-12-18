31b6886bba7fbc3ee8020e7e0a6d67c09e3324bd
Chris Brown <cdb186000@gmail.com>
2019-12-18 16:00:01 -0500

    Initial Check-in

---
+++ src/main/java/com/chrisbrown/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.chrisbrown.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "/index"})
+	public String getIndexPage() {
+		System.out.println("Test Test");
+		return "index";
+	}
+}
